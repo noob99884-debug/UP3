5055b2a270dd95b253deb65ffbc6e37638e990f7
noob99884-debug <noob99884@gmail.com>
2025-09-01 19:01:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1559,7 +1559,7 @@ async def universal_drm_handler(bot: Client, m: Message):
 
             elif "classplusapp" in url:
                 signed_api = f"https://covercel.vercel.app/extract_keys?url={url}@bots_updatee&user_id={user_id}"
-                response = requests.get(signed_api, timeout=60)
+                response = requests.get(signed_api, timeout=120)
                 url = response.text.strip()
                 url = response.json()['url']  
                 
